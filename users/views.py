19c5b6b72a59cf89df264f86d4349f5fde0cd861
Alex <zayac880880@mail.ru>
2023-10-20 16:13:31 +0300

    I did it!!!!

--- users/views.py
+++ users/views.py
@@ -1,6 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+
+
+class CustomPermission(permissions.BasePermission):
+    """
+    Пользователи могут выполнять регистрацию и авторизацию без аутентификации,
+    но для других операций требуется аутентификация.
+    """
+    def has_permission(self, request, view):
+        if view.action in ['create', 'token']:
+            return True
+        return request.user and request.user.is_authenticated
 
 
 class UsersViewSet(viewsets.ModelViewSet):
@@ -10,6 +24,17 @@ class UsersViewSet(viewsets.ModelViewSet):
     Он наследует viewsets.ModelViewSet из Django REST framework и предоставляет
     стандартные CRUD-операции для модели User.
     """
+    permission_classes = [CustomPermission]
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
+    def create(self, request, *args, **kwargs):
+
+        email = request.data.get('email')
+        password = request.data.get('password')
+        hashed_password = make_password(password)
+        User = get_user_model()
+
+        user = User(email=email, password=hashed_password)
+        user.save()
+        return Response({'message': 'Пользователь успешно зарегистрирован!'}, status=status.HTTP_201_CREATED)
