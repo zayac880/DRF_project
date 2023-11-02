55596aedd346991b3d9f44411fe3e3e95a9f262c
Alex <zayac880880@mail.ru>
2023-11-03 00:25:56 +0300

    I did it!!!!

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY_SETTING_DJANGO')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
